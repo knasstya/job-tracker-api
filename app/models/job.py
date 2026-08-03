eb6933f8dd45d746a0d4283d58f58db01cb8e492
Nastassia Kazhemiakina <nastassiakazhemiakina@gmail.com>
2026-08-03 15:30:16 +0300

    Create job database model and migration

---
+++ app/models/job.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime, timezone
+
+from app.core.database import Base
+
+
+class Job(Base):
+    __tablename__ = "jobs"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    company = Column(String, nullable=False)
+
+    position = Column(String, nullable=False)
+
+    status = Column(
+        String,
+        default="Applied"
+    )
+
+    created_at = Column(
+    DateTime,
+    default=lambda: datetime.now(timezone.utc)
+)
